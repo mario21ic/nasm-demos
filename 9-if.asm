2da29276586cacd6cec11382ffd8fe2e5d389989
Mario Inga <mario21ic@gmail.com>
2021-05-31 04:56:44 -0500

    feat(global) adding if example

---
+++ 9-if.asm
@@ -0,0 +1,43 @@
+section .data
+    linea db 0xA, 0xD
+
+    par db 0xA,0xD, "Es ", 0xA, 0xD
+    par_len equ $-par
+
+    impar db 0xA,0xD, "Es impar", 0xA, 0xD
+    impar_len equ $-impar
+
+section .bss
+    rpta resb 1
+
+section .text
+    global _start
+
+_start:
+    mov eax, 5
+    mov ebx, 9
+
+    ;cmp eax, ebx
+    cmp eax, 5 ; va a modificar zero flag
+    jz cuerpo_if
+
+    jmp salir
+
+cuerpo_if:
+    mov ebx, 1
+
+salir:
+    add ebx, 48 ; ascii
+    mov [rpta], ebx
+
+    ; Imprimir en pantalla
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, rpta
+    mov edx, 1
+    int 0x80
+
+    ; Finalizar
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
